3c88fc98eeb7be00f4998d3088a92ad53ea941ba
James Pier <jpier2012@gmail.com>
2019-05-28 20:37:12 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,17 @@
 class Dog
+  @@all = []
+  
   attr_accessor :name, :breed, :age
 
   def initailize(name:, breed:, age:)
     @name = name
     @breed = breed
     @age = age
+    @@all << self
+  end
+
+  self.all
+    @@all
   end
 
 end
